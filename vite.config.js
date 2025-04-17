b1bc6a592b3947023818ad4cf1736de93fb7f082
chrissmeltzer <smeltz.c@gmail.com>
2025-04-16 21:41:40 -0500

    fix: github pages commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: '/grimoire/', // Replace 'grimoire' with your repository name
 });
